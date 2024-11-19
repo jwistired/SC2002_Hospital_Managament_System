953c67b277667aff1d37b619bac794fbf00872b8
jwistired <seowjingwoon@outlook.com>
2024-11-19 23:04:37 +0800

    updated script

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@ rmdir /s /q bin
 mkdir bin
 
 echo Compiling Java files...
-javac -Xlint:unchecked -d bin -sourcepath src main/*.java controllers/*.java views/*.java models/*.java utils/*.java
+javac -Xlint:-unchecked -d bin -sourcepath src main/*.java controllers/*.java views/*.java models/*.java utils/*.java
 
 if %errorlevel% neq 0 (
     echo Compilation failed.
--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ rm -rf bin
 mkdir bin
 
 echo "Compiling Java files with detailed warnings..."
-javac -Xlint:unchecked -d bin -sourcepath src main/*.java controllers/*.java views/*.java models/*.java utils/*.java
+javac -Xlint:-unchecked -d bin -sourcepath src main/*.java controllers/*.java views/*.java models/*.java utils/*.java
 
 if [ $? -ne 0 ]; then
     echo "Compilation failed."
